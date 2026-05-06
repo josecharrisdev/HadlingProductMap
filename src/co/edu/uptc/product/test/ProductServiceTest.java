85bfe2e419f10c58f19a4f36544385299b36c11a
josecharrisdev <josecharrisdev@gmail.com>
2026-05-06 06:51:20 -0500

    Se crearon las primeras pruebas unitarias

--- src/co/edu/uptc/product/test/ProductServiceTest.java
+++ src/co/edu/uptc/product/test/ProductServiceTest.java
@@ -2,13 +2,40 @@ package co.edu.uptc.product.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import co.edu.uptc.product.domain.Product;
+import co.edu.uptc.product.repository.ProductRepositoryImpl;
+import co.edu.uptc.product.service.ProductService;
+
 class ProductServiceTest {
+	
+	private ProductService productService;
+
+	@BeforeEach
+	public void init() {
+		this.productService = new ProductService(new ProductRepositoryImpl());
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testMethodAddProductSuccessful() {
+		/* Creación de los datos requeridos para la prueba */
+		Product product1 = new Product(1L, "Producto 1", 1200d);
+		Product productCreated = this.productService.addProduct(product1);
+		
+		Assert.assertEquals(product1, productCreated);
+	}
+	
+	@Test
+	void testMethodAddProductFailed() {
+		/* Creación de los datos requeridos para la prueba */
+		Product product1 = new Product(1L, "Producto 1", 1200d);
+		Product product2 = new Product(1L, "Producto 1", 1200d);
+		this.productService.addProduct(product1);
+		Product productCreated2 = this.productService.addProduct(product2);
+		Assert.assertNull(productCreated2);
 	}
 
 }
